818d5cf6b71c294a633535c7e8f4aa092296d6ef
Matt Ellicott <mattellicott@gmail.com>
2021-10-09 06:11:18 -0500

    Lesson-2 partial

--- RB101-109/Lesson-2/mortgage_car_loan_calculator.rb
+++ RB101-109/Lesson-2/mortgage_car_loan_calculator.rb
@@ -1,21 +1,44 @@
-def valid_number?(string, int=0, duration='false')
-  until int.positive?
-    print "Enter a valid #{string}: "
-    int = gets.to_f
-    break if (int >= 0) && (duration == 'true')
-  end
-  int
+require 'yaml'
+PROMPTS = YAML.load_file('mortgage_car_loan_calculator.yml')
+
+def prompt(message)
+  print(message)
 end
 
-loan_amount = valid_number?('loan amount')
-loan_apr = (valid_number?('Annual Percentage Rage (APR)(e.g., 6)') / 12) / 100
-loan_duration_years = valid_number?('loan duration in Years', 0, 'true')
-loan_duration_months = valid_number?('remainder of the loan duration in Months',
-                                     0, 'true')
+def get_valid_number(duration = 'false')
+  num = ''
+  loop do
+    num = gets.chomp
+    num = case num.scan(/\D/)
+          when []
+            num.to_f
+          else
+            -1
+          end
+    # Break if num is 0 and a duration
+    break if (num == 0) && (duration == 'true')
+    # Otherwise, break if num is positive
+    break if num.positive?
+    prompt(PROMPTS['invalid'])
+  end
+  num
+end
 
-loan_duration = (loan_duration_years * 12) + loan_duration_months
-monthly_payment = loan_amount * (loan_apr / (1 - (1 + loan_apr)**(-loan_duration)))
+# Prompt for, and retrieve data
+prompt(PROMPTS['loan'])
+loan_amount = get_valid_number()
+prompt(PROMPTS['apr'])
+loan_apr = (get_valid_number() / 12) / 100
+prompt(PROMPTS['years'])
+loan_duration_years = get_valid_number('true')
+prompt(PROMPTS['months'])
+loan_duration_months = get_valid_number('true')
 
+# Perform Calculations
+loan_duration = ((loan_duration_years * 12) + loan_duration_months).to_i
+monthly_payment = (loan_amount * (loan_apr / (1 - (1 + loan_apr)**(-loan_duration)))).round(2)
+
+# Print results
 puts "\nMonthly interest rate: #{format('%11.10s', loan_apr)}%"
-puts "Loan duration: #{format('%13.10s', loan_duration.to_i)} months"
-puts "Monthly payment: #{format('%18.11s', "\$#{monthly_payment.round(2)}")}"
+puts "Loan duration: #{format('%13.10s', loan_duration)} months"
+puts "Monthly payment: #{format('%18.11s', "\$#{monthly_payment}")}"
